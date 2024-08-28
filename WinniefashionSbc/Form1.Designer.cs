7ff4ddaff2f8858ef3e533b5eabeffa33cfbad53
ASH4805 <asswinenair@gmail.com>
2024-08-28 11:28:54 +0800

    Add files via upload

---
+++ WinniefashionSbc/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace WinniefashionSbc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tab = new System.Windows.Forms.TabControl();
+            this.tabPage_pID = new System.Windows.Forms.TabPage();
+            this.textBox_MIX = new System.Windows.Forms.TextBox();
+            this.label_COLORMIXCH = new System.Windows.Forms.Label();
+            this.lblSummary = new System.Windows.Forms.Label();
+            this.listBox_summary = new System.Windows.Forms.ListBox();
+            this.button_new = new System.Windows.Forms.Button();
+            this.lbl_color = new System.Windows.Forms.Label();
+            this.comboBox_color = new System.Windows.Forms.ComboBox();
+            this.lblpieces = new System.Windows.Forms.Label();
+            this.richTextBox_pieces = new System.Windows.Forms.RichTextBox();
+            this.textBox_Price = new System.Windows.Forms.TextBox();
+            this.textBox_quantity = new System.Windows.Forms.TextBox();
+            this.textBox_BOXNO = new System.Windows.Forms.TextBox();
+            this.textBox_PulanamID = new System.Windows.Forms.TextBox();
+            this.BTN_CLICK = new System.Windows.Forms.Button();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQTY = new System.Windows.Forms.Label();
+            this.lblbox = new System.Windows.Forms.Label();
+            this.lblPulanamId = new System.Windows.Forms.Label();
+            this.Tab.SuspendLayout();
+            this.tabPage_pID.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tab
+            // 
+            this.Tab.Controls.Add(this.tabPage_pID);
+            this.Tab.Location = new System.Drawing.Point(12, 12);
+            this.Tab.Name = "Tab";
+            this.Tab.SelectedIndex = 0;
+            this.Tab.Size = new System.Drawing.Size(1332, 617);
+            this.Tab.TabIndex = 2;
+            // 
+            // tabPage_pID
+            // 
+            this.tabPage_pID.Controls.Add(this.textBox_MIX);
+            this.tabPage_pID.Controls.Add(this.label_COLORMIXCH);
+            this.tabPage_pID.Controls.Add(this.lblSummary);
+            this.tabPage_pID.Controls.Add(this.listBox_summary);
+            this.tabPage_pID.Controls.Add(this.button_new);
+            this.tabPage_pID.Controls.Add(this.lbl_color);
+            this.tabPage_pID.Controls.Add(this.comboBox_color);
+            this.tabPage_pID.Controls.Add(this.lblpieces);
+            this.tabPage_pID.Controls.Add(this.richTextBox_pieces);
+            this.tabPage_pID.Controls.Add(this.textBox_Price);
+            this.tabPage_pID.Controls.Add(this.textBox_quantity);
+            this.tabPage_pID.Controls.Add(this.textBox_BOXNO);
+            this.tabPage_pID.Controls.Add(this.textBox_PulanamID);
+            this.tabPage_pID.Controls.Add(this.BTN_CLICK);
+            this.tabPage_pID.Controls.Add(this.lblPrice);
+            this.tabPage_pID.Controls.Add(this.lblQTY);
+            this.tabPage_pID.Controls.Add(this.lblbox);
+            this.tabPage_pID.Controls.Add(this.lblPulanamId);
+            this.tabPage_pID.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_pID.Name = "tabPage_pID";
+            this.tabPage_pID.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_pID.Size = new System.Drawing.Size(1324, 591);
+            this.tabPage_pID.TabIndex = 0;
+            this.tabPage_pID.Text = "PULANAM ID";
+            this.tabPage_pID.UseVisualStyleBackColor = true;
+            // 
+            // textBox_MIX
+            // 
+            this.textBox_MIX.BackColor = System.Drawing.Color.Lavender;
+            this.textBox_MIX.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_MIX.Location = new System.Drawing.Point(922, 46);
+            this.textBox_MIX.Multiline = true;
+            this.textBox_MIX.Name = "textBox_MIX";
+            this.textBox_MIX.Size = new System.Drawing.Size(173, 56);
+            this.textBox_MIX.TabIndex = 18;
+            // 
+            // label_COLORMIXCH
+            // 
+            this.label_COLORMIXCH.AutoSize = true;
+            this.label_COLORMIXCH.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_COLORMIXCH.Location = new System.Drawing.Point(925, 8);
+            this.label_COLORMIXCH.Name = "label_COLORMIXCH";
+            this.label_COLORMIXCH.Size = new System.Drawing.Size(156, 33);
+            this.label_COLORMIXCH.TabIndex = 19;
+            this.label_COLORMIXCH.Text = "MIX COLOR :";
+            // 
+            // lblSummary
+            // 
+            this.lblSummary.AutoSize = true;
+            this.lblSummary.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSummary.Location = new System.Drawing.Point(230, 112);
+            this.lblSummary.Name = "lblSummary";
+            this.lblSummary.Size = new System.Drawing.Size(135, 33);
+            this.lblSummary.TabIndex = 17;
+            this.lblSummary.Text = "Summary :";
+            // 
+            // listBox_summary
+            // 
+            this.listBox_summary.BackColor = System.Drawing.Color.FloralWhite;
+            this.listBox_summary.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox_summary.ForeColor = System.Drawing.Color.MediumBlue;
+            this.listBox_summary.FormattingEnabled = true;
+            this.listBox_summary.HorizontalScrollbar = true;
+            this.listBox_summary.ItemHeight = 29;
+            this.listBox_summary.Location = new System.Drawing.Point(227, 148);
+            this.listBox_summary.Name = "listBox_summary";
+            this.listBox_summary.ScrollAlwaysVisible = true;
+            this.listBox_summary.Size = new System.Drawing.Size(546, 410);
+            this.listBox_summary.TabIndex = 16;
+            // 
+            // button_new
+            // 
+            this.button_new.BackColor = System.Drawing.Color.Violet;
+            this.button_new.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_new.Location = new System.Drawing.Point(795, 290);
+            this.button_new.Name = "button_new";
+            this.button_new.Size = new System.Drawing.Size(286, 69);
+            this.button_new.TabIndex = 15;
+            this.button_new.Text = "NEW ITEM";
+            this.button_new.UseVisualStyleBackColor = false;
+            this.button_new.Click += new System.EventHandler(this.button_new_Click_1);
+            // 
+            // lbl_color
+            // 
+            this.lbl_color.AutoSize = true;
+            this.lbl_color.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_color.Location = new System.Drawing.Point(6, 126);
+            this.lbl_color.Name = "lbl_color";
+            this.lbl_color.Size = new System.Drawing.Size(89, 33);
+            this.lbl_color.TabIndex = 14;
+            this.lbl_color.Text = "Color :";
+            // 
+            // comboBox_color
+            // 
+            this.comboBox_color.BackColor = System.Drawing.Color.Cornsilk;
+            this.comboBox_color.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_color.FormattingEnabled = true;
+            this.comboBox_color.Location = new System.Drawing.Point(12, 162);
+            this.comboBox_color.Name = "comboBox_color";
+            this.comboBox_color.Size = new System.Drawing.Size(184, 41);
+            this.comboBox_color.TabIndex = 13;
+            // 
+            // lblpieces
+            // 
+            this.lblpieces.AutoSize = true;
+            this.lblpieces.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpieces.Location = new System.Drawing.Point(1106, 17);
+            this.lblpieces.Name = "lblpieces";
+            this.lblpieces.Size = new System.Drawing.Size(69, 33);
+            this.lblpieces.TabIndex = 12;
+            this.lblpieces.Text = "PCS :";
+            // 
+            // richTextBox_pieces
+            // 
+            this.richTextBox_pieces.BackColor = System.Drawing.Color.Azure;
+            this.richTextBox_pieces.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox_pieces.Location = new System.Drawing.Point(1101, 49);
+            this.richTextBox_pieces.Name = "richTextBox_pieces";
+            this.richTextBox_pieces.Size = new System.Drawing.Size(199, 53);
+            this.richTextBox_pieces.TabIndex = 11;
+            this.richTextBox_pieces.Text = "";
+            // 
+            // textBox_Price
+            // 
+            this.textBox_Price.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox_Price.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Price.Location = new System.Drawing.Point(464, 49);
+            this.textBox_Price.Multiline = true;
+            this.textBox_Price.Name = "textBox_Price";
+            this.textBox_Price.Size = new System.Drawing.Size(199, 56);
+            this.textBox_Price.TabIndex = 9;
+            // 
+            // textBox_quantity
+            // 
+            this.textBox_quantity.BackColor = System.Drawing.Color.Thistle;
+            this.textBox_quantity.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_quantity.Location = new System.Drawing.Point(227, 49);
+            this.textBox_quantity.Multiline = true;
+            this.textBox_quantity.Name = "textBox_quantity";
+            this.textBox_quantity.Size = new System.Drawing.Size(199, 56);
+            this.textBox_quantity.TabIndex = 7;
+            // 
+            // textBox_BOXNO
+            // 
+            this.textBox_BOXNO.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox_BOXNO.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_BOXNO.Location = new System.Drawing.Point(6, 49);
+            this.textBox_BOXNO.Multiline = true;
+            this.textBox_BOXNO.Name = "textBox_BOXNO";
+            this.textBox_BOXNO.Size = new System.Drawing.Size(199, 56);
+            this.textBox_BOXNO.TabIndex = 5;
+          //  this.textBox_BOXNO.TextChanged += new System.EventHandler(this.textBox_BOXNO_TextChanged);
+            // 
+            // textBox_PulanamID
+            // 
+            this.textBox_PulanamID.BackColor = System.Drawing.Color.Lavender;
+            this.textBox_PulanamID.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_PulanamID.Location = new System.Drawing.Point(717, 49);
+            this.textBox_PulanamID.Multiline = true;
+            this.textBox_PulanamID.Name = "textBox_PulanamID";
+            this.textBox_PulanamID.Size = new System.Drawing.Size(199, 56);
+            this.textBox_PulanamID.TabIndex = 3;
+            // 
+            // BTN_CLICK
+            // 
+            this.BTN_CLICK.BackColor = System.Drawing.Color.Honeydew;
+            this.BTN_CLICK.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_CLICK.Location = new System.Drawing.Point(795, 162);
+            this.BTN_CLICK.Name = "BTN_CLICK";
+            this.BTN_CLICK.Size = new System.Drawing.Size(286, 69);
+            this.BTN_CLICK.TabIndex = 0;
+            this.BTN_CLICK.Text = "ADD";
+            this.BTN_CLICK.UseVisualStyleBackColor = false;
+            this.BTN_CLICK.Click += new System.EventHandler(this.BTN_CLICK_Click_1);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(458, 13);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(138, 33);
+            this.lblPrice.TabIndex = 10;
+            this.lblPrice.Text = "Unit Price :";
+            // 
+            // lblQTY
+            // 
+            this.lblQTY.AutoSize = true;
+            this.lblQTY.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQTY.Location = new System.Drawing.Point(230, 13);
+            this.lblQTY.Name = "lblQTY";
+            this.lblQTY.Size = new System.Drawing.Size(162, 33);
+            this.lblQTY.TabIndex = 8;
+            this.lblQTY.Text = "BX.Quantity :";
+            // 
+            // lblbox
+            // 
+            this.lblbox.AutoSize = true;
+            this.lblbox.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblbox.Location = new System.Drawing.Point(6, 15);
+            this.lblbox.Name = "lblbox";
+            this.lblbox.Size = new System.Drawing.Size(102, 33);
+            this.lblbox.TabIndex = 6;
+            this.lblbox.Text = "Box No:";
+            // 
+            // lblPulanamId
+            // 
+            this.lblPulanamId.AutoSize = true;
+            this.lblPulanamId.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPulanamId.Location = new System.Drawing.Point(711, 8);
+            this.lblPulanamId.Name = "lblPulanamId";
+            this.lblPulanamId.Size = new System.Drawing.Size(155, 33);
+            this.lblPulanamId.TabIndex = 4;
+            this.lblPulanamId.Text = "Pulanam ID :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1367, 702);
+            this.Controls.Add(this.Tab);
+            this.Name = "Form1";
+            this.Text = "ORDER_PLACEMENT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Tab.ResumeLayout(false);
+            this.tabPage_pID.ResumeLayout(false);
+            this.tabPage_pID.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Tab;
+        private System.Windows.Forms.TabPage tabPage_pID;
+        private System.Windows.Forms.Label lblSummary;
+        private System.Windows.Forms.ListBox listBox_summary;
+        private System.Windows.Forms.Button button_new;
+        private System.Windows.Forms.Label lbl_color;
+        private System.Windows.Forms.ComboBox comboBox_color;
+        private System.Windows.Forms.Label lblpieces;
+        private System.Windows.Forms.RichTextBox richTextBox_pieces;
+        private System.Windows.Forms.TextBox textBox_Price;
+        private System.Windows.Forms.TextBox textBox_quantity;
+        private System.Windows.Forms.TextBox textBox_BOXNO;
+        private System.Windows.Forms.TextBox textBox_PulanamID;
+        private System.Windows.Forms.Button BTN_CLICK;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQTY;
+        private System.Windows.Forms.Label lblbox;
+        private System.Windows.Forms.Label lblPulanamId;
+        private System.Windows.Forms.TextBox textBox_MIX;
+        private System.Windows.Forms.Label label_COLORMIXCH;
+    }
+}
+
